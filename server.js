30b6d7719ccb00e93d955870e4c9818dbc6d6375
kovacsnandor <kovacsnandor@gmail.com>
2023-02-14 11:14:02 +0100

    mysql connect ok

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const mysql = require("mysql");
 const app = express();
 
 app.get('/cars', (req, res) => {
-  res.send('Hello World');
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'taxi6'
+  });
+
+  connection.connect();
+
+  const sql = 'SELECT * FROM cars';
+  connection.query(sql, (error, results, fields)=>{
+    if (error) {
+        console.log(error);
+        return;
+    }
+    res.send(results);
+  })
+
+  connection.end();
+
 });
 
 app.listen(3000, ()=>{
