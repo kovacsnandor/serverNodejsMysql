35a5649203487408d1791b9a8f2774aa98d7ff09
kovacsnandor <kovacsnandor@gmail.com>
2023-02-21 09:15:55 +0100

    connection javítás

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 require("dotenv").config();
 
-
-const express = require('express');
+const express = require("express");
 const mysql = require("mysql");
 const app = express();
 
 const pool = require("./config/database.js");
 
-app.get('/cars', (req, res) => {
-  const sql = 'SELECT * FROM cars';
-  pool.query(sql, (error, results, fields)=>{
+app.get("/cars", (req, res) => {
+  
+  pool.getConnection(function (error, connection) {
     if (error) {
-        console.log(error);
-        return;
+      console.log("szerver hiba");
+      return;
     }
-    res.send(results);
-  })
-
+    const sql = "SELECT * FROM cars";
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        console.log("sql hiba");
+        return;
+      }
+      res.send(results);
+    });
+    connection.release();
+  });
 });
 
-app.listen(process.env.APP_PORT, ()=>{
-    console.log(`Data server, listen port: ${process.env.APP_PORT}`);
-})
-  
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Data server, listen port: ${process.env.APP_PORT}`);
+});
